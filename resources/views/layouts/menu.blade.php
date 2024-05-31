9bc89d687a5df8c681f137ad147059992172b4d0
thiran-naleendra <60607966+thiran-naleendra@users.noreply.github.com>
2024-05-31 21:38:27 +0530

    first commit
    update admin lte ui

---
+++ resources/views/layouts/menu.blade.php
@@ -0,0 +1,40 @@
+<nav class="mt-2">
+    <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+      <!-- Add icons to the links using the .nav-icon class
+           with font-awesome or any other icon font library -->
+      
+      <li class="nav-item">
+        <a href="pages/widgets.html" class="nav-link">
+          <i class="nav-icon fas fa-th"></i>
+          <p>
+            Widgets
+          </p>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a href="pages/widgets.html" class="nav-link">
+          <i class="nav-icon fas fa-th"></i>
+          <p>
+            test1
+          </p>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a href="pages/widgets.html" class="nav-link">
+          <i class="nav-icon fas fa-th"></i>
+          <p>
+            test2
+          </p>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a href="pages/widgets.html" class="nav-link">
+          <i class="nav-icon fas fa-th"></i>
+          <p>
+            test3
+          </p>
+        </a>
+      </li>
+      
+    </ul>
+  </nav>
---
+++ routes/web.php
@@ -0,0 +1,7 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\MainController;
+
+Route::get('/' , [MainController::class,'index'])->name('main');
