95ae7300aa29ee12ace0b64fd1bcf5fc02ceaaea
jayani <jayani.nawarathna@inovaitsys.com>
2024-06-02 11:50:43 +0530

    profile content

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -10,6 +10,10 @@
             border-color: #BA68C8;
         }
 
+        .content {
+            margin-top: -3rem;
+        }
+
         .profile-button {
             background: #BA68C8;
             box-shadow: none;
@@ -34,55 +38,105 @@
             color: #682773;
             cursor: pointer;
         }
+
+        .tabcontent {
+            display: none;
+        }
     </style>
     <br>
     <section class="content">
         <div class="container rounded bg-white mt-5">
             <div class="row">
                 <div class="col-md-4 border-right">
-                    <div class="d-flex flex-column align-items-center text-center p-3 py-5"><img class="rounded-circle mt-5"
-                            src="https://i.imgur.com/0eg0aG0.jpg" width="90"><span class="font-weight-bold">John
-                            Doe</span><span class="text-black-50">john_doe12@bbb.com</span><span>United States</span></div>
+                    <div class="d-flex flex-column align-items-center text-center p-3 py-5">
+                        <img class="rounded-circle mt-5" src="https://i.imgur.com/0eg0aG0.jpg" width="90">
+                        <span class="font-weight-bold">John Doe</span>
+                        <span class="text-black-50">john_doe12@bbb.com</span>
+                        <span>United States</span>
+                    </div>
                 </div>
                 
                 <div class="col-md-8">
                     <div class="p-3 py-5">
-                        <div class="d-flex justify-content-between align-items-center mb-3">
-                            <div class="d-flex flex-row align-items-center back"><i
-                                    class="fa fa-long-arrow-left mr-1 mb-1"></i>
-                                <h6>Back to home</h6>
+                        <div class="tab d-flex justify-content-between align-items-center mb-3">
+                            <button class="btn btn-primary tablinks" onclick="openCity(event, 'view')">View Profile</button>
+                            <button class="btn btn-primary tablinks" onclick="openCity(event, 'edit')">Edit Profile</button>
+                        </div>
+                        <div id="view" class="tabcontent">
+                        <div class="row mt-3">
+                                <div class="col-md-3"><label>Name</label></div>
+                                <div class="col-md-9">John Doe</div>
                             </div>
-                            <h6 class="text-right">Edit Profile</h6>
+                            <div class="row mt-3">
+                                <div class="col-md-3"><label>Username</label></div>
+                                <div class="col-md-9">John Doe</div>
+                            </div>
+                            <div class="row mt-3">
+                                <div class="col-md-3"><label>Email</label></div>
+                                <div class="col-md-9">john_doe12@bbb.com</div>
+                            </div>
+                            <div class="row mt-3">
+                                <div class="col-md-3"><label>Links</label></div>
+                                <div class="col-md-9"></div>
+                            </div>
+                            <div class="row mt-3">
+                                <div class="col-md-3"><label>Bio</label></div>
+                                <div class="col-md-9"></div>
+                            </div>
                         </div>
-                        <div class="row mt-2">
-                            <div class="col-md-6"><input type="text" class="form-control" placeholder="first name"
-                                    ></div>
-                            <div class="col-md-6"><input type="text" class="form-control" 
-                                    placeholder=""></div>
+                        <div id="edit" class="tabcontent">
+                            <div class="row mt-3">
+                                <div class="col-md-3"><label>Name</label></div>
+                                <div class="col-md-9">
+                                    <input type="text" class="form-control" placeholder="Name" >
+                                </div>
+                            </div>
+                            <div class="row mt-3">
+                                <div class="col-md-3"><label>Username</label></div>
+                                <div class="col-md-9">
+                                    <input type="text" class="form-control" placeholder="Username">
+                                </div>
+                            </div>
+                            <div class="row mt-3">
+                                <div class="col-md-3"><label>Email</label></div>
+                                <div class="col-md-9">
+                                    <input type="text" class="form-control" placeholder="Email">
+                                </div>
+                            </div>
+                            <div class="row mt-3">
+                                <div class="col-md-3"><label>Links</label></div>
+                                <div class="col-md-9">
+                                    <textarea class="form-control" rows="2" placeholder="Links"></textarea>
+                                </div>
+                            </div>
+                            <div class="row mt-3">
+                                <div class="col-md-3"><label>Bio</label></div>
+                                <div class="col-md-9">
+                                    <textarea class="form-control" rows="2" placeholder="Bio"></textarea>
+                                </div>
+                            </div>
+                            <div class="mt-5 text-right">
+                                <button class="btn btn-primary profile-button" type="button">Save Profile</button>
+                            </div>
                         </div>
-                        <div class="row mt-3">
-                            <div class="col-md-6"><input type="text" class="form-control" placeholder="Email"
-                                    ></div>
-                            <div class="col-md-6"><input type="text" class="form-control" 
-                                    placeholder="Phone number"></div>
-                        </div>
-                        <div class="row mt-3">
-                            <div class="col-md-6"><input type="text" class="form-control" placeholder="address"
-                                    ></div>
-                            <div class="col-md-6"><input type="text" class="form-control" 
-                                    placeholder="Country"></div>
-                        </div>
-                        <div class="row mt-3">
-                            <div class="col-md-6"><input type="text" class="form-control" placeholder="Bank Name"
-                                    ></div>
-                            <div class="col-md-6"><input type="text" class="form-control" 
-                                    placeholder="Account Number"></div>
-                        </div>
-                        <div class="mt-5 text-right"><button class="btn btn-primary profile-button" type="button">Save
-                                Profile</button></div>
                     </div>
                 </div>
             </div>
         </div>
+        <script>
+            function openCity(evt, cityName) {
+                var i, tabcontent, tablinks;
+                tabcontent = document.getElementsByClassName("tabcontent");
+                for (i = 0; i < tabcontent.length; i++) {
+                    tabcontent[i].style.display = "none";
+                }
+                tablinks = document.getElementsByClassName("tablinks");
+                for (i = 0; i < tablinks.length; i++) {
+                    tablinks[i].className = tablinks[i].className.replace(" active", "");
+                }
+                document.getElementById(cityName).style.display = "block";
+                evt.currentTarget.className += " active";
+            }
+        </script>
     </section>
 @endsection
