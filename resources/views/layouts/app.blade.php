8fc1e60422aafd15d868b68e43e5ad949a132d5d
jayani <jayani.nawarathna@inovaitsys.com>
2024-06-03 19:04:54 +0530

    demo removed

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -173,7 +173,6 @@
     <script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
     <script src="plugins/bs-custom-file-input/bs-custom-file-input.min.js"></script>
     <script src="dist/js/adminlte.min.js"></script>
-    <script src="dist/js/demo.js"></script>
     <script src="plugins/jquery-mousewheel/jquery.mousewheel.js"></script>
     <script src="plugins/raphael/raphael.min.js"></script>
     <script src="plugins/jquery-mapael/jquery.mapael.min.js"></script>
