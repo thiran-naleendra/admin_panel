bd4efaeca71804c6e081cfb74cc3441e2cf3f300
jayani <jayani.nawarathna@inovaitsys.com>
2024-06-04 18:07:39 +0530

    make req ui done

--- app/Http/Controllers/RequestController.php
+++ app/Http/Controllers/RequestController.php
@@ -8,10 +8,24 @@ class RequestController extends Controller
 {
     public function index()
     {
-        return view('create_request');
+        // Retrieve select options from the configuration file
+        $job_types = config('selectOptions.job_types');
+        $soil_test = config('selectOptions.soil_test');
+        $surveys = config('selectOptions.surveys');
+        $feature_surveys = config('selectOptions.feature_surveys');
+        $ahd_ffl = config('selectOptions.ahd_ffl');
+        $other_jobs = config('selectOptions.other_jobs');
+        return view('create_request', compact('job_types','soil_test','surveys','feature_surveys','ahd_ffl','other_jobs'));
     }
     public function view_request()
     {
-        return view('view_request');
+        // Retrieve select options from the configuration file
+        $job_types = config('selectOptions.job_types');
+        $soil_test = config('selectOptions.soil_test');
+        $surveys = config('selectOptions.surveys');
+        $feature_surveys = config('selectOptions.feature_surveys');
+        $ahd_ffl = config('selectOptions.ahd_ffl');
+        $other_jobs = config('selectOptions.other_jobs');
+        return view('view_request', compact('job_types','soil_test','surveys','feature_surveys','ahd_ffl','other_jobs'));
     }
 }
---
+++ config/selectOptions.php
@@ -0,0 +1,48 @@
+<?php
+// config/select_options.php
+
+return [
+    'job_types' => [
+        '' => '',
+        'ST' => 'Soil test',
+        'SU' => 'Survey',
+        'IN' => 'Inspection',
+        'OJ' => 'Other Jobs',
+        // Add more options as needed
+    ],
+    'soil_test' => [
+        '' => '',
+        'PRDT' => 'Pre Demolished Test',
+        'PODT' => 'Post Demolished Test',
+        'FP' => 'Footing Prob',
+        'OJ' => 'Other Jobs',
+        // Add more options as needed
+    ],
+    'surveys' => [
+        '' => '',
+        'FS' => 'Feature Survey',
+        'AHD' => 'AHD - FFL indicator level to Plumbing riser',
+        'RE' => 'Reastablishment',
+        // Add more options as needed
+    ],
+    'feature_surveys' => [
+        '' => '',
+        'PRFS' => 'Pre Demolition Feature Survey',
+        'POFS' => 'Post Demolition Feature Survey',
+        // Add more options as needed
+    ],
+    'ahd_ffl' => [
+        '' => '',
+        'PRFS' => 'Pre Pour Mark',
+        'POFS' => 'Post Pour Confirmation',
+        // Add more options as needed
+    ],
+    'other_jobs' => [
+        '' => '',
+        'J1' => 'Job 1',
+        'J2' => 'Job 2',
+        'J3' => 'Job 3',
+        // Add more options as needed
+    ],
+    // Add more select fields as needed
+];
