46c16b1fcc2b3fcb9d8176732b4ba3d25cc2bb04
jayani <jayani.nawarathna@inovaitsys.com>
2024-06-17 05:29:30 +0530

    styles for make request

--- config/selectOptions.php
+++ config/selectOptions.php
@@ -3,7 +3,7 @@
 
 return [
     'request_types' => [
-        '' => '',
+        '' => 'Select a option',
         'ST' => 'Soil test',
         'SU' => 'Survey',
         'IN' => 'Inspection',
@@ -11,7 +11,7 @@ return [
         // Add more options as needed
     ],
     'soil_test' => [
-        '' => '',
+        '' => 'Select a option',
         'PRDT' => 'Pre Demolished Test',
         'PODT' => 'Post Demolished Test',
         'FP' => 'Footing Prob',
@@ -19,26 +19,26 @@ return [
         // Add more options as needed
     ],
     'surveys' => [
-        '' => '',
+        '' => 'Select a option',
         'FS' => 'Feature Survey',
         'AHD' => 'AHD - FFL indicator level to Plumbing riser',
         'RE' => 'Reastablishment',
         // Add more options as needed
     ],
     'feature_surveys' => [
-        '' => '',
+        '' => 'Select a option',
         'PRFS' => 'Pre Demolition Feature Survey',
         'POFS' => 'Post Demolition Feature Survey',
         // Add more options as needed
     ],
     'ahd_ffl' => [
-        '' => '',
+        '' => 'Select a option',
         'PRFS' => 'Pre Pour Mark',
         'POFS' => 'Post Pour Confirmation',
         // Add more options as needed
     ],
     'other_jobs' => [
-        '' => '',
+        '' => 'Select a option',
         'J1' => 'Job 1',
         'J2' => 'Job 2',
         'J3' => 'Job 3',
